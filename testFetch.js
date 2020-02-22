0c17fa9beacd4d33277ffa0e682d3a5841fb2881
maryapple <maria9ku@gmail.com>
2020-02-22 12:55:46 +0300

    The points on map are taken from json on api.myjson.com

---
+++ testFetch.js
@@ -0,0 +1,55 @@
+// getResource = () => {
+//     const res = fetch('https://swapi.co/api/people')
+//         .then((resp) => resp.json())
+//     return res
+// }
+
+// const finalRes = getResource()
+// console.log(finalRes)
+
+
+const res = fetch('https://drive.google.com/open?id=1BQ0tsy-K_88WdPuJN4GjFKJkbY4nQiEA')
+    .then(res => res.json())
+    .then(res => console.log(res))
+
+
+
+
+
+
+// const __data__ = {}
+
+// getResource = async (__data__) => {
+//     const url = 'https://drive.google.com/open?id=1BQ0tsy-K_88WdPuJN4GjFKJkbY4nQiEA'
+    /*const res = await fetch(url
+    // , {
+    //     method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    //     mode: 'cors', // no-cors, cors, *same-origin
+    //     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    //     credentials: 'same-origin', // include, *same-origin, omit
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         // 'Content-Type': 'application/x-www-form-urlencoded',
+    //     },
+    //     redirect: 'follow', // manual, *follow, error
+    //     referrer: 'no-referrer', // no-referrer, *client
+    //     body: JSON.stringify(__data__), // тип данных в body должен соответвовать значению заголовка "Content-Type"
+    // }
+    )
+        .then(response => response.json())
+
+    if (!res.ok) {
+        throw new Error(`could not fetch ${url} , recieved ${res.status}`)
+    }
+    console.log(res.json())
+    return res.json()*/
+
+
+
+
+    // const response = await fetch('http://example.com/movies.json')
+    // const myJson = await response.json();
+    // console.log(JSON.stringify(myJson));
+// }
+
+// getResource(__data__)
