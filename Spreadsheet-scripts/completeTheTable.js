94f3cf6da9102e39c670ba9c27309ea5284f8e3d
maryapple <maria9ku@gmail.com>
2020-03-05 22:53:35 +0300

    added links to spreadsheet as json

--- Spreadsheet-scripts/completeTheTable.js
+++ Spreadsheet-scripts/completeTheTable.js
@@ -1,53 +1,9 @@
 var app = SpreadsheetApp
 var initialSheet = app.getActiveSpreadsheet().getSheetByName('Answers from Form')
 var linkSheet = app.getActiveSpreadsheet().getSheetByName("Links")
-var mapSheet = app.getActiveSpreadsheet().getSheetByName("Map")
-var placesSheet = app.getActiveSpreadsheet().getSheetByName("Places")
+// var mapSheet = app.getActiveSpreadsheet().getSheetByName("Map")
+// var placesSheet = app.getActiveSpreadsheet().getSheetByName("Places")
 var form = FormApp.openById('1NfHSL2y2U1zkx-pkNH9KSYxF8KzshdOS_bqdDPm3nX4')
-
-// Сюда приходит ОДИН ПОСЛЕДНИЙ ответ из формы
-function onSubmit(e){
-	// Массив ответов на форму
-	var responses = e.response.getItemResponses()
-	Logger.log(responses[0].getResponse(), responses[1].getResponse(), responses[2].getResponse(), responses[3].getResponse())
-
-	completeMapSheet(responses)
-	completePlaces(responses)
-}
-
-function completeMapSheet(responses) {
-	var lastRowMap = mapSheet.getLastRow() + 1
-	var name = responses[0].getResponse().toString()
-	var region = responses[1].getResponse().toString()
-	var authors = responses[2].getResponse().toString()
-	var year = responses[3].getResponse().toString()
-	var description = responses[4].getResponse().toString()
-	var photo = responses[5].getResponse().toString()
-	var photoLink = 'https://drive.google.com/open?id=' + photo
-	var address = responses[6].getResponse().toString()
-	var db = responses[7].getResponse().toString()
-	var nameOfDataset = name + ' (' + authors + ', ' + year + ')'
-
-	mapSheet.getRange('A' + lastRowMap).setValue(nameOfDataset)
-	mapSheet.getRange('B' + lastRowMap).setValue(region)
-	mapSheet.getRange('C' + lastRowMap).setValue(address)
-	mapSheet.getRange('D' + lastRowMap).setValue(description + ' plots')
-	mapSheet.getRange('E' + lastRowMap).setValue(photoLink)
-}
-
-function completePlaces(responses) {
-	var lastRowMPlaces = placesSheet.getLastRow() + 1
-	var name = responses[0].getResponse().toString()
-	var authors = responses[2].getResponse().toString()
-	var year = responses[3].getResponse().toString()
-	var nameOfDataset = name + ' (' + authors + ', ' + year + ')'
-
-	var nameToLink = name.toLowerCase()
-	nameToLink = nameToLink.replace(/\s+/g,'')
-	var hyperLink = '=hyperlink("https://sites.google.com/auditory.ru/avarus/main/' + nameToLink + '"; "' + nameOfDataset + '")'
-
-	placesSheet.getRange('A' + lastRowMPlaces).setValue(hyperLink)
-}
 
 // Add a trigger which listens for form submit
 function addTrigger() {
@@ -57,19 +13,49 @@ function addTrigger() {
         .create();
 }
 
-/* function addBlock () {
-	var mySite = 'https://sites.google.com/auditory.ru/avatest/main';
-	var myDomain = 'auditory.ru'
-	var site1 = SitesApp.getSite(myDomain)
-	var myPages = site1.getAllDescendants();
-	for (i in myPages) {
-		Logger.log('page=' + myPages[i].getTitle());
-	}  
-	var site = SitesApp.getSiteByUrl(mySite);
-	// var site = SitesApp.getSite('auditory.ru', 'Ava Rus');
-	var page = site.getChildren()[0];
+// Сюда приходит ОДИН ПОСЛЕДНИЙ ответ из формы
+function onSubmit(e){
+	// Массив ответов на форму
+	var responses = e.response.getItemResponses()
+	// Logger.log(responses[0].getResponse(), responses[1].getResponse(), responses[2].getResponse(), responses[3].getResponse())
 
-	var sitesByD = SitesApp.getSites('auditory.ru');
-	Logger.log(sitesByD)
-	page.setHtmlContent('index.html');
-} */
+	sendResponse(responses)
+	// completePlaces(responses)
+}
+
+function sendResponse(responses) {
+	
+	var name = responses[0].getResponse().toString()
+	var region = responses[1].getResponse().toString()
+	var authors = responses[2].getResponse().toString()
+	var year = responses[3].getResponse().toString()
+	var plots = responses[4].getResponse().toString() + ' plots'
+	var photo = responses[5].getResponse().toString()
+	// var photoLink = 'https://drive.google.com/open?id=' + photo
+	var coordinates = responses[6].getResponse().toString()
+	var dbLink = responses[7].getResponse().toString()
+	var title = name + ' (' + authors + ', ' + year + ')'
+	var description = `<p style='font-weight: 600; font-size: 0.9rem;'>` + title + `</p><p>` + plots + 
+		`</p><iframe src='https://drive.google.com/file/d/` + photo + `/preview' width='100%'></iframe>`
+
+	// var lastRowMap = mapSheet.getLastRow() + 1
+	// mapSheet.getRange('A' + lastRowMap).setValue(nameOfDataset)
+	// mapSheet.getRange('B' + lastRowMap).setValue(region)
+	// mapSheet.getRange('C' + lastRowMap).setValue(address)
+	// mapSheet.getRange('D' + lastRowMap).setValue(plots + ' plots')
+	// mapSheet.getRange('E' + lastRowMap).setValue(photoLink)
+}
+
+// function completePlaces(responses) {
+// 	var lastRowMPlaces = placesSheet.getLastRow() + 1
+// 	var name = responses[0].getResponse().toString()
+// 	var authors = responses[2].getResponse().toString()
+// 	var year = responses[3].getResponse().toString()
+// 	var nameOfDataset = name + ' (' + authors + ', ' + year + ')'
+
+// 	var nameToLink = name.toLowerCase()
+// 	nameToLink = nameToLink.replace(/\s+/g,'')
+// 	var hyperLink = '=hyperlink("https://sites.google.com/auditory.ru/avarus/main/' + nameToLink + '"; "' + nameOfDataset + '")'
+
+// 	placesSheet.getRange('A' + lastRowMPlaces).setValue(hyperLink)
+// }
--- Spreadsheet-scripts/test.js
+++ Spreadsheet-scripts/test.js
@@ -1,36 +1,71 @@
-function myFunction() {
-    var currentSpreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-    var targetSheet = currentSpreadsheet.getSheetByName("Скачать");
-    var emptyRow = targetSheet.getLastRow()
-    Logger.log(emptyRow)
-  }
-  
-  
-  function deleteSpaces(){
-    var str = 'aaa vvv ccc'
-    Logger.log(str.replace(/\s+/g, ''))
-  }
-  
-  function test1() {
-    var sites = SitesApp.getSites();
-    for(var i in sites) {
-       Logger.log(sites[i].getUrl());
-    }
-  }
-  
-  function ex(){
-  var pageStart = 0;
-  var pageSize = 10;
-  while (true) {
-    Logger.log("Loading sites starting at %s", pageStart);
-    var sites = SitesApp.getAllSites("example.com", pageStart, pageSize);
-    if (sites.length == 0) {
-      break;
-    }
-    Logger.log("Got %s sites back", sites.length);
-    pageStart += sites.length;
-    for(var i in sites) {
-      Logger.log("Found site: %s", sites[i].getUrl());
-    }
-  }
+// просмотр ВСЕХ ответов формы (https://developers.google.com/apps-script/reference/forms/form-response)
+function getResponsesFromForm() {
+	var form = FormApp.openById('1NfHSL2y2U1zkx-pkNH9KSYxF8KzshdOS_bqdDPm3nX4')
+	var formResponses = form.getResponses();
+	for (var i = 0; i < formResponses.length; i++) {
+		var formResponse = formResponses[i];
+		var itemResponses = formResponse.getItemResponses();
+		for (var j = 0; j < itemResponses.length; j++) {
+			var itemResponse = itemResponses[j];
+			Logger.log('Response #%s to the question "%s" was "%s"',
+			(i + 1).toString(),
+			itemResponse.getItem().getTitle(),
+			itemResponse.getResponse());
+		}
+	}
 }
+
+
+function myFunction() {
+	var currentSpreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+	var targetSheet = currentSpreadsheet.getSheetByName("Скачать");
+	var emptyRow = targetSheet.getLastRow()
+	Logger.log(emptyRow)
+}
+
+
+function deleteSpaces() {
+	var str = 'aaa vvv ccc'
+	Logger.log(str.replace(/\s+/g, ''))
+}
+
+function test1() {
+	var sites = SitesApp.getSites();
+	for (var i in sites) {
+		Logger.log(sites[i].getUrl());
+	}
+}
+
+function ex() {
+	var pageStart = 0;
+	var pageSize = 10;
+	while (true) {
+		Logger.log("Loading sites starting at %s", pageStart);
+		var sites = SitesApp.getAllSites("example.com", pageStart, pageSize);
+		if (sites.length == 0) {
+			break;
+		}
+		Logger.log("Got %s sites back", sites.length);
+		pageStart += sites.length;
+		for (var i in sites) {
+			Logger.log("Found site: %s", sites[i].getUrl());
+		}
+	}
+}
+
+/* function addBlock () {
+	var mySite = 'https://sites.google.com/auditory.ru/avatest/main';
+	var myDomain = 'auditory.ru'
+	var site1 = SitesApp.getSite(myDomain)
+	var myPages = site1.getAllDescendants();
+	for (i in myPages) {
+		Logger.log('page=' + myPages[i].getTitle());
+	}  
+	var site = SitesApp.getSiteByUrl(mySite);
+	// var site = SitesApp.getSite('auditory.ru', 'Ava Rus');
+	var page = site.getChildren()[0];
+
+	var sitesByD = SitesApp.getSites('auditory.ru');
+	Logger.log(sitesByD)
+	page.setHtmlContent('index.html');
+} */
