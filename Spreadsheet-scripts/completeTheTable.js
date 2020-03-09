27140d9b00fffb1c4f205eceb1746be21320bab0
maryapple <maria9ku@gmail.com>
2020-03-10 00:15:49 +0300

    Fixed function with writing down info on a map sheet

--- Spreadsheet-scripts/completeTheTable.js
+++ Spreadsheet-scripts/completeTheTable.js
@@ -1,7 +1,7 @@
 var app = SpreadsheetApp
 var initialSheet = app.getActiveSpreadsheet().getSheetByName('Answers from Form')
 var linkSheet = app.getActiveSpreadsheet().getSheetByName("Links")
-// var mapSheet = app.getActiveSpreadsheet().getSheetByName("Map")
+var mapSheet = app.getActiveSpreadsheet().getSheetByName("Map")
 // var placesSheet = app.getActiveSpreadsheet().getSheetByName("Places")
 var form = FormApp.openById('1NfHSL2y2U1zkx-pkNH9KSYxF8KzshdOS_bqdDPm3nX4')
 
@@ -24,26 +24,31 @@ function onSubmit(e){
 }
 
 function sendResponse(responses) {
-	
 	var name = responses[0].getResponse().toString()
 	var region = responses[1].getResponse().toString()
 	var authors = responses[2].getResponse().toString()
 	var year = responses[3].getResponse().toString()
 	var plots = responses[4].getResponse().toString() + ' plots'
-	var photo = responses[5].getResponse().toString()
-	// var photoLink = 'https://drive.google.com/open?id=' + photo
+	var photoId = responses[5].getResponse().toString()
 	var coordinates = responses[6].getResponse().toString()
 	var dbLink = responses[7].getResponse().toString()
 	var title = name + ' (' + authors + ', ' + year + ')'
-	var description = `<p style='font-weight: 600; font-size: 0.9rem;'>` + title + `</p><p>` + plots + 
-		`</p><iframe src='https://drive.google.com/file/d/` + photo + `/preview' width='100%'></iframe>`
+	var description = `<p>` + plots + `</p><iframe src='https://drive.google.com/file/d/` 
+		+ photoId + `/preview' width='100%'></iframe>`
 
-	// var lastRowMap = mapSheet.getLastRow() + 1
-	// mapSheet.getRange('A' + lastRowMap).setValue(nameOfDataset)
-	// mapSheet.getRange('B' + lastRowMap).setValue(region)
-	// mapSheet.getRange('C' + lastRowMap).setValue(address)
-	// mapSheet.getRange('D' + lastRowMap).setValue(plots + ' plots')
-	// mapSheet.getRange('E' + lastRowMap).setValue(photoLink)
+	var lastRowMap = mapSheet.getLastRow() + 1
+	mapSheet.getRange('A' + lastRowMap).setValue(title)
+	mapSheet.getRange('B' + lastRowMap).setValue(description)
+	mapSheet.getRange('C' + lastRowMap).setValue(name)
+	mapSheet.getRange('D' + lastRowMap).setValue(coordinates)
+	mapSheet.getRange('E' + lastRowMap).setValue(region)
+	mapSheet.getRange('F' + lastRowMap).setValue(authors)
+	mapSheet.getRange('G' + lastRowMap).setValue(year)
+	mapSheet.getRange('H' + lastRowMap).setValue(plots)
+	// где хранится фото? права доступа?
+	mapSheet.getRange('I' + lastRowMap).setValue(photoId)
+	// не на скачивание?
+	mapSheet.getRange('J' + lastRowMap).setValue(dbLink)
 }
 
 // function completePlaces(responses) {
